2b29a94d2f973d654b528c264ea4b610e6cb0d4c
Arthur Roma <lopestui88@gmail.com>
2025-01-24 22:53:36 -0300

    associacao category com product no config

--- src/main/java/com/educandoweb/course/entities/Product.java
+++ src/main/java/com/educandoweb/course/entities/Product.java
@@ -28,10 +28,10 @@ public class Product implements Serializable {
 	private Double price;
 	private String imgUrl;
 	
-//	@ManyToMany
-//	@JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
+	@ManyToMany
+	@JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>();
-//	
+
 //	@OneToMany(mappedBy = "id.product")
 //	private Set<OrderItem> items = new HashSet<>();
 	public Product() {
